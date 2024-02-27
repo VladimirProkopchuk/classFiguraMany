842774581d8a253321eca86e9be7903b3b0220ec
Vladimir Prokopchuk <vovan.devil999@gmail.com>
2024-02-27 16:48:41 +0300

    Second commit

--- Figura_func/Figura_func.cpp
+++ Figura_func/Figura_func.cpp
@@ -3,80 +3,74 @@
 #include <string>
 using namespace std;
 
-class _4ugol {
+class fourgolnik {
 private:
     int a, b, c, d;
     int A, B, C, D;
     string name = "Четырехугольник";
 public:
-    _4ugol(int _a, int _b, int _c, int _d, int _A, int _B, int _C, int _D) {
+    fourgolnik(int _a, int _b, int _c, int _d, int _A, int _B, int _C, int _D) {
         a = _a; b = _b; c = _c; d = _d;
         A = _A; B = _B; C = _C; D = _D;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+   virtual void print() {
+        cout << name << endl;
     }
-    void print() {
-        //cout << "Фигура " << name << endl;
-        cout << "Сторона a = " << a << " Сторона b = " << b << " Сторона c = " << c << " Сторона d = " << d << endl;
-        cout << "Угол A = " << A << " Угол B = " << B << " Угол C = " << C << " Угол D = " << D << endl;
-    }
+   void info() {
+       cout << "Сторона a = " << a << " Сторона b = " << b << " Сторона c = " << c << " Сторона d = " << d << endl;
+       cout << "Угол A = " << A << " Угол B = " << B << " Угол C = " << C << " Угол D = " << D << endl;
+   }
 };
 
-class paralelogram : public _4ugol {
+class paralelogram : public fourgolnik {
 private:
     string name = "Параллелограмм";
     int a, b, A, B;
 public:
-    paralelogram(int _a, int _b, int _A, int _B) : _4ugol(_a, _b, _a, _b, _A, _B, _A, _B) {
+    paralelogram(int _a, int _b, int _A, int _B) : fourgolnik(_a, _b, _a, _b, _A, _B, _A, _B) {
         a = _a; b = _b; A = _A; B = _B;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+    void print() override {
+        cout << name << endl;
     }
 };
 
-class romb :public _4ugol {
+class romb :public fourgolnik {
 private:
     string name = "Ромб";
     int a, A, B;
 public:
-    romb(int _a, int _A, int _B) : _4ugol(_a, _a, _a, _a, _A, _B, _A, _B) {
+    romb(int _a, int _A, int _B) : fourgolnik(_a, _a, _a, _a, _A, _B, _A, _B) {
         a = _a; A = _A; B = _B;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+    void print() override {
+        cout << name << endl;
     }
 };
 
-class Pryamougolnik : public _4ugol {
+class Pryamougolnik : public fourgolnik {
 private:
     int a, b;
     string name = "Прямоугольник";
 public:
-    Pryamougolnik(int _a, int _b) : _4ugol(_a, _b, _a, _b, 90, 90, 90, 90) {
+    Pryamougolnik(int _a, int _b) : fourgolnik(_a, _b, _a, _b, 90, 90, 90, 90) {
         a = _a; b = _b;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+    void print() override {
+        cout << name << endl;
     }
 };
 
-class Kvadrat : public _4ugol {
+class Kvadrat : public fourgolnik {
 private:
     int a;
     string name = "Квадрат";
 public:
-    Kvadrat(int _a) : _4ugol(_a, _a, _a, _a, 90, 90, 90, 90) {
+    Kvadrat(int _a) : fourgolnik(_a, _a, _a, _a, 90, 90, 90, 90) {
         a = _a;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+    void print() override {
+        cout << name << endl;
     }
 };
 
@@ -86,17 +80,15 @@ private:
     int a = 0;  int b = 0;  int c = 0;
     int A = 0; int B = 0; int C = 0;
 public:
-    
-    Tringle(int _a, int _b, int _c, int _A, int _B, int _C) {
+   Tringle(int _a, int _b, int _c, int _A, int _B, int _C) {
         a = _a; b = _b; c = _c;
         A = _A; B = _B; C = _C;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+
+    virtual void print() {
+        cout << name << endl;
     }
-    void print() {
-    
+   void info (){
         cout << "Сторонa a = " << a << " Сторонa b = " << b << " Сторонa c = " << c << endl;
         cout << "Угол A = " << A << " Угол B = " << B << " Угол C = " << C << endl;
     }
@@ -112,11 +104,10 @@ public:
         a = _a; b = _b; c = _c;
         A = _A; B = _B;
     }
-   void print_all() {
-       cout << name << ": " << endl;
-       print();
-   }
- };
+   void print() override {
+       cout << name << endl;
+    }
+   };
 
 class RavBedTringle : public Tringle {
 private:
@@ -128,9 +119,8 @@ public:
         a = _a; b = _b;
         A = _A; B = _B;
     }
-    void print_all() {
-        cout << name << ": " << endl;
-        print();
+    void print() override {
+        cout << name << endl;
     }
 };
 
@@ -141,33 +131,45 @@ private:
 public:
    RavSTringle(int _a) : Tringle(_a, _a, _a, 60, 60 ,60) {
         a = _a; 
-    }
-   void print_all() {
-       cout << name << ": " << endl;
-       print();
+    }  
+   void print() override {
+       cout << name << endl;
    }
 };
 
+void print_all(Tringle* t) {
+    t->print();
+    t->info();
+    for (int i = 0; i < 35; i++) {
+        cout << "-";
+    } cout << endl;
+}
+void print_all(fourgolnik* f) {
+    f->print();
+    f->info();
+    for (int i = 0; i < 35; i++) {
+        cout << "-";
+    } cout << endl;
+}
 
 int main() {
     setlocale(LC_ALL, "russian");
     Tringle tringle(3, 4, 5, 60, 90, 30);
-    tringle.print_all();
+    print_all(&tringle);
     PryaTringle Prt(3, 4, 5, 50, 60);
-    Prt.print_all();
+    print_all(&Prt);
     RavBedTringle rbt(4, 5, 50, 60);
-    rbt.print_all();
+    print_all(&rbt);
     RavSTringle ravSTringle(5);
-    ravSTringle.print_all();
-    _4ugol fig(5, 4, 6, 4, 60, 70, 120, 30);
-    fig.print_all();
+    print_all(&ravSTringle);
+    fourgolnik fig(5, 4, 6, 4, 60, 70, 120, 30);
+    print_all(&fig);
     paralelogram prl(4, 5, 70, 130);
-    prl.print_all();
+    print_all(&prl);
     romb rmb(5, 60, 70);
-    rmb.print_all();
+    print_all(&rmb);
     Kvadrat kvd(10);
-    kvd.print_all();
+    print_all(&kvd);
     Pryamougolnik prm(14, 6);
-    prm.print_all();
-
+    print_all(&prm);
 }
